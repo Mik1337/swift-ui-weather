05564be9d949c1aad7c2f8fc7cf8e43195e02b5d
Mik <mik23@protonmail.ch>
2023-11-15 00:05:03 +0530

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by mik on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
